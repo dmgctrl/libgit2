521d683f2f8eede831959bda4c47f824905dc057
Dmitry Golomidov <dmitry.golomidov@gmail.com>
2013-06-04 13:42:16 -0700

    still error out when GIT_ENOTFOUND

--- src/push.c
+++ src/push.c
@@ -309,9 +309,14 @@ static int revwalk(git_vector *commits, git_push *push)
 
 			error = git_merge_base(&base, push->repo,
 					       &spec->loid, &spec->roid);
+			if (error == GIT_ENOTFOUND) {
+				giterr_set(GITERR_REFERENCE,
+					"Cannot push non-fastforwardable reference");
+				error = GIT_ENONFASTFORWARD;
+				goto on_error;
+			}
 
-			if (error == GIT_ENOTFOUND ||
-				(!error && !git_oid_equal(&base, &spec->roid))) {
+			if ((!error && !git_oid_equal(&base, &spec->roid))) {
 				spec->rejected = true;
 			}
 
